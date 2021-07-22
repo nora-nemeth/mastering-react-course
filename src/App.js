537673c14749ce1603e6f4538202c9f99b55397f
nora-nemeth <nemethnora1996@gmail.com>
2021-07-22 13:54:18 +0200

    Add navBar component and modify component tree

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
+import React, { Component } from 'react';
+import NavBar from './components/navbar';
+import Counters from './components/counters';
 import './App.css';
 
-function App() {
-  return <div className="App"></div>;
+class App extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <NavBar />
+        <main className="container">
+          <Counters />
+        </main>
+      </React.Fragment>
+    );
+  }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,11 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.css';
-import Counters from './components/counters';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Counters />
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
