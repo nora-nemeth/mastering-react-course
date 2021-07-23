50b670b39d1bc99d956a6acd4e3617ffb361579f
nora-nemeth <nemethnora1996@gmail.com>
2021-07-23 14:43:32 +0200

    Add decrement button and reformat layout

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -4,20 +4,31 @@ class Counter extends Component {
   render() {
     return (
       <React.Fragment>
-        <div>
-          <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
-          <button
-            onClick={() => this.props.onIncrement(this.props.counter)}
-            className="btn btn-secondary btn-sm"
-          >
-            Increment
-          </button>
-          <button
-            onClick={() => this.props.onDelete(this.props.counter.id)}
-            className="btn btn-danger btn-sm m-2"
-          >
-            Delete
-          </button>
+        <div className="row">
+          <div className="col-1 align-self-center">
+            <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+          </div>
+          <div className="col-11 align-self-center">
+            <button
+              onClick={() => this.props.onIncrement(this.props.counter)}
+              className="btn btn-secondary btn-sm"
+            >
+              +
+            </button>
+            <button
+              disabled={this.props.counter.value === 0}
+              onClick={() => this.props.onDecrement(this.props.counter)}
+              className="btn btn-secondary btn-sm m-2"
+            >
+              -
+            </button>
+            <button
+              onClick={() => this.props.onDelete(this.props.counter.id)}
+              className="btn btn-danger btn-sm"
+            >
+              Delete
+            </button>
+          </div>
         </div>
       </React.Fragment>
     );
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,3 +1,5 @@
+import React from 'react';
+
 //Stateless functional component:
 
 const NavBar = ({ totalCounters }) => {
